25e50749954974388302ef6287b4c6332a16db03
jaromanp <33042735+jaromanp@users.noreply.github.com>
2020-05-06 11:41:50 -0500

    Implementación de las pruebas unitarias completada

--- tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -6,17 +6,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
-});
-
 class LoginControllerTest extends TestCase
 {
     /** @test para verificacion de la vista login */
---
+++ tests/Feature/Http/Controllers/ItemControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\User;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ItemControllerTest extends TestCase
+{
+    /** @test para comprobar que solo usuarios autenticados pueden ver la pagina de inicio */
+    public function item_index_display_all_products(){
+        $response = $this->get('/item/index');
+        $response->assertSee('All products');
+    }
+
+}
