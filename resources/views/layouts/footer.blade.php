deca3da0eb4d04f9b43d4da49dc79dfac04017cc
amartinezv <aleja.vega13@gmail.com>
2020-05-11 23:49:55 -0500

    link de vistas en el footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -22,8 +22,8 @@
           <div class="col-xs-6 col-md-3">
             <h6>Quick Links</h6>
             <ul class="footer-links">
-              <li><a href="http://scanfcode.com/about/">About Us</a></li>
-              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+              <li><a href="{{ route('home.about') }}">About Us</a></li>
+              <li><a href="{{ route('home.contact') }}">Contact Us</a></li>
               <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
               <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
               <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
